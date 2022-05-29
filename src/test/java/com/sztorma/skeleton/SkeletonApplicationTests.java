330147d7dc073a5e78b238e27762db459a9fb1b4
sztorma <sztorma@outlook.com>
2022-05-29 18:16:23 +0200

    init project

---
+++ src/test/java/com/sztorma/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sztorma.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
