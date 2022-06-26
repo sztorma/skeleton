3607b4450bfce0dc4a73b392e26772781ee7888d
sztorma <sztorma@outlook.com>
2022-06-26 13:57:37 +0200

    jwt token assertion

--- src/test/java/com/sztorma/skeleton/security/config/AuthenticationIT.java
+++ src/test/java/com/sztorma/skeleton/security/config/AuthenticationIT.java
@@ -1,5 +1,8 @@
 package com.sztorma.skeleton.security.config;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,14 +29,26 @@ public class AuthenticationIT {
     @Test
     @DisplayName("Succesful authentication")
     public void testAuthenticateSuccess() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.post(
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post(
                 "/api/authenticate") //
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED) //
                 .param("username", "Admin") //
                 .param("password", "admin") //
         )
-                .andExpect(MockMvcResultMatchers.status().isOk());
-        // TODO token validation
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        JSONObject content = new JSONObject(result.getResponse().getContentAsString());
+        assertEquals("Admin", getJwtUsername(content.getString("access_token")));
+        assertEquals("Admin", getJwtUsername(content.getString("refresh_token")));
+    }
+
+    private String getJwtUsername(String token) {
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        String username = decodedJWT.getSubject();
+        return username;
+
     }
 
     @Test
