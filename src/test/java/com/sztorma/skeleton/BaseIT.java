768bbb496f24b3ce36e9567119540cff47fa449c
sztorma <sztorma@outlook.com>
2022-08-21 12:54:56 +0200

    implement testcontainers

---
+++ src/test/java/com/sztorma/skeleton/BaseIT.java
@@ -0,0 +1,29 @@
+package com.sztorma.skeleton;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Testcontainers
+public class BaseIT {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:latest");
+
+    @DynamicPropertySource
+
+    static void postgreSQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
+}
--- src/test/java/com/sztorma/skeleton/security/config/AuthenticationIT.java
+++ src/test/java/com/sztorma/skeleton/security/config/AuthenticationIT.java
@@ -6,8 +6,6 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,10 +16,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+import com.sztorma.skeleton.BaseIT;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class AuthenticationIT {
+public class AuthenticationIT extends BaseIT {
 
     @Autowired
     private MockMvc mockMvc;
